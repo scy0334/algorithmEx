e5d4ec5c2367707f91e4fbdf29f244ac055e4a7b
Si Choi <sichoi@Siui-MacBook-Pro.local>
2020-08-30 19:34:43 +0900

    quickSort completed

--- algorithExercise/sort/quickSort.js
+++ algorithExercise/sort/quickSort.js
@@ -1,9 +1,19 @@
-const swap = (arr, a, b) => {
-    let temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-}
-
 const quickSort = (arr) => {
-    return arr;
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivot = arr[0];
+    const leftArr = [];
+    const rightArr = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            leftArr.push(arr[i]);
+        } else {
+            rightArr.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
 }
